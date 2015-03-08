109b5eac968fe7aae2f478cc3f0cf2761925ae9d
Stefan Sundin <stefan@stefansundin.com>
2015-03-08 15:09:48 -0700

    Support building 32-bit.

--- build.bat
+++ build.bat
@@ -1,15 +1,18 @@
 @echo off
 
-set prefix64=x86_64-w64-mingw32-
+set prefix=x86_64-w64-mingw32-
+
+:: To compile 32-bit, uncomment this:
+:: set prefix=i686-w64-mingw32-
 
 taskkill /F /IM ElevatedStartup.exe
 
-%prefix64%windres include/resource.rc include/resource.o
+%prefix%windres include/resource.rc include/resource.o
 
 if "%1" == "debug" (
-  %prefix64%gcc -o ElevatedStartup.exe elevatedstartup.c include/resource.o -mwindows -lole32 -g -DDEBUG
+  %prefix%gcc -o ElevatedStartup.exe elevatedstartup.c include/resource.o -mwindows -lole32 -g -DDEBUG
 ) else (
-  %prefix64%gcc -o ElevatedStartup.exe elevatedstartup.c include/resource.o -mwindows -lole32 -O2 -s
+  %prefix%gcc -o ElevatedStartup.exe elevatedstartup.c include/resource.o -mwindows -lole32 -O2 -s
 
   if "%1" == "run" (
     start ElevatedStartup.exe %2 %3 %4 %5 %6 %7 %8 %9
